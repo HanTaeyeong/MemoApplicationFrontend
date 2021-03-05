2d72e9f68632e95d0c83eadb4bd129ff72a8a723
HanTaeyeong <gksxodud2721@naver.com>
2021-03-05 18:42:16 +0900

    with proxy eddited

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,16 +1,17 @@
 import client from './client';
 
+const proxy='/simplememo.com/api/auth';
 
 export const login = async ({ username, password }: { username: string, password: string }) =>
-    await client.post('/api/auth/login', { username, password });
+    await client.post(proxy+'/login', { username, password });
 
 export const logout = async () => {
-    return await client.post('/api/auth/logout')
+    return await client.post(proxy+'/logout')
 };
 
 export const register = async ({ username, password }: { username: string, password: string }) =>
-    await client.post('/api/auth/register', { username, password })
+    await client.post(proxy+'/register', { username, password })
 
-export const check = async () => await client.get('/api/auth/check');
+export const check = async () => await client.get(proxy+'/check');
 
 
--- src/lib/api/post.ts
+++ src/lib/api/post.ts
@@ -1,17 +1,17 @@
 import client from './client';
 
-export const getPostList = ({ page, limit }: { page: number, limit: number }) => client.get(`/api/post/get?page=${page}&limit=${limit}`)
+const proxy='/simplememo.com/api/post'
+
+export const getPostList = ({ page, limit }: { page: number, limit: number }) => client.get(`${proxy}/get?page=${page}&limit=${limit}`)
 
 export const writePost = ({ title, contents, tags }: { title: string, contents: string, tags: string[] | undefined }) =>
-    client.post('/api/post/write', { title, contents, tags });
+    client.post(proxy+'/write', { title, contents, tags });
 
 export const readPost = (id: string) =>
-    client.get(`/api/post/read/${id}`);
+    client.get(`${proxy}/read/${id}`);
 
 export const deletePost = (id: string) =>
-    client.delete(`/api/post/delete/${id}`);
+    client.delete(`${proxy}/delete/${id}`);
 
 export const updatePost = ({ _id, title, contents, tags }: { _id: string, title: string, contents: string, tags: string[] | undefined }) =>
-    client.patch(`/api/post/update/${_id}`, { title, contents, tags })
-
-
+    client.patch(`${proxy}/update/${_id}`, { title, contents, tags })
