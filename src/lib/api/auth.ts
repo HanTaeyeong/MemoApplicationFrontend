8c771d5f9c7827372966839773b3e3ecb0b3776c
HanTaeyeong <gksxodud2721@naver.com>
2021-03-05 17:05:25 +0900

    proxy

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,5 +1,6 @@
 import client from './client';
 
+
 export const login = async ({ username, password }: { username: string, password: string }) =>
     await client.post('/api/auth/login', { username, password });
 
