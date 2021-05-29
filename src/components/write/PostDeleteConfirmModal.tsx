e66df5b488bf995d084a600ac707aada0066db1a
HanTaeyeong <gksxodud2721@naver.com>
2021-05-29 19:57:43 +0900

    unify apicall method and prevent empty title

--- src/components/write/PostDeleteConfirmModal.tsx
+++ src/components/write/PostDeleteConfirmModal.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import styled from 'styled-components';
 import ConfirmModal from '../common/ConfirmModal';
 
-
 const PostDeleteConfirmModal = ({visible}:{visible:boolean}) => {
     return (
         <ConfirmModal visible={visible} title={'Delete memo'} description={'Are you sure to delete this memo?'}
