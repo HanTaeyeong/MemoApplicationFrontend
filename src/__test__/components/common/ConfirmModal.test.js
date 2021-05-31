f97448c1ec19873244a346843cf9141b4997d525
HanTaeyeong <rex@quebon.com>
2021-05-31 15:18:14 +0900

    confirmModal render test

--- src/__test__/components/common/ConfirmModal.test.js
+++ src/__test__/components/common/ConfirmModal.test.js
@@ -1,5 +1,50 @@
+import { Provider } from "react-redux";
 
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
-it('re',()=>{
-    expect(1).toBe(1);
-})
+import configureMockStore from "redux-mock-store";
+
+import ConfirmModal from "../../../components/common/ConfirmModal";
+
+const store = configureMockStore([])({});
+
+describe("common/ConfirmModal test", () => {
+    
+  it("ConfirmModal renders well", () => {
+    const visible = true;
+    const confirmModal = render(
+      <Provider store={store}>
+        <ConfirmModal
+          visible={visible}
+          title={"Delete memo"}
+          description={"Are you sure to delete this memo?"}
+          confirmText={"Yes"}
+          cancelText={"No"}
+        />
+      </Provider>
+    );
+
+    expect(screen.getByText(/Delete memo/i)).toBeTruthy();
+    expect(screen.getByText(/Are you sure to delete this memo/i)).toBeTruthy();
+    expect(screen.getByText(/Yes/i)).toBeTruthy();
+    expect(screen.getByText(/No/i)).toBeTruthy();
+  });
+
+  it("confirmModal with visible props false not renders", () => {
+    const visible = false;
+    const confirmModal = render(
+      <Provider store={store}>
+        <ConfirmModal
+          visible={visible}
+          title={"Delete memo"}
+          description={"Are you sure to delete this memo?"}
+          confirmText={"Yes"}
+          cancelText={"No"}
+        />
+      </Provider>
+    );
+    
+    expect(confirmModal.child).not.toBeDefined();
+  });
+});
