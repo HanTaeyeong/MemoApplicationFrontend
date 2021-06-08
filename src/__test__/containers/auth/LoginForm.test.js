cad7683a1e16cac1d2b7b12c66627d0814325588
HanTaeyeong <rex@quebon.com>
2021-06-08 16:52:27 +0900

    with LoginForm test

---
+++ src/__test__/containers/auth/LoginForm.test.js
@@ -0,0 +1,80 @@
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import fetchMock from "fetch-mock";
+import { Provider } from "react-redux";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+import { render, screen, act, fireEvent } from "@testing-library/react";
+
+import { setItem, getItem } from "../../../lib/localStorageRequest";
+import LoginForm from "../../../containers/auth/LoginForm";
+
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const history = createBrowserHistory();
+
+const renderLoginForm = (store) =>
+  render(
+    <Provider store={store}>
+      <Router history={history}>
+        <LoginForm />
+      </Router>
+    </Provider>
+  );
+
+describe("LoginForm", () => {
+  it("LoginForm render test with localstorage username", async () => {
+    await setItem("username", "wanderertyty123");
+    const store = mockStore({ auth: {}, loading: {}, write: {} });
+    await renderLoginForm(store);
+    const expectResult = [
+      {
+        type: "auth/CHANGE_FIELD",
+        payload: {
+          authType: undefined,
+          username: undefined,
+          password: undefined,
+          passwordConfirm: undefined,
+          authErrorMessage: "",
+        },
+      },
+      { type: "auth/CHECK" },
+      { type: "loading/START_LOADING", payload: "auth/CHECK" },
+    ];
+
+    expect(store.getActions()).toEqual(expectResult);
+    await setItem("username", "");
+  });
+
+  it("LoginForm render test without localstorage username", async () => {
+    const store = mockStore({ auth: {}, loading: {}, write: {} });
+    await renderLoginForm(store);
+    const expectResult = [
+      {
+        type: "auth/CHANGE_FIELD",
+        payload: {
+          authType: undefined,
+          username: undefined,
+          password: undefined,
+          passwordConfirm: undefined,
+          authErrorMessage: "",
+        },
+      },
+    ];
+
+    expect(store.getActions()).toEqual(expectResult);
+  });
+
+  it('StyledInput focus test',()=>{
+    const store = mockStore({ auth: {}, loading: {}, write: {} });
+    renderLoginForm(store);
+    
+    const styledInput= screen.getByRole('input');
+    styledInput.focus();
+
+    expect(1).toEqual(1);
+  })
+  
+});
